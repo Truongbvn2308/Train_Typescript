e239392af940f52e47e2298ef1fb956358155f61
Truongbvn2308 <truongbvn@runsystem.net>
2025-04-24 16:59:40 +0700

    12344445

--- basic.js
+++ basic.js
@@ -9,7 +9,7 @@ function logAndThrow(errorMessage) {
   throw new Error(errorMessage);
 }
 function typeFunction(fn) {
-  fn("akl"); //sdfsdsfdjkhbjkhbbj
+  fn("akl"); //sdfsdsfdjkhbjkhbbjxzsdgsfdgsdfd11111
 }
 typeFunction(log);
 console.log(add(0, 1));
